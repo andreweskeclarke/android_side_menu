9c130e4fb30d60bad7827d5d122f17326f390ed9
andreweskeclarke <andreweskeclarke@gmail.com>
2013-05-16 11:43:28 -0500

    Side menu and action bar working

--- src/com/aeclarke/androidsidemenu/MainActivity.java
+++ src/com/aeclarke/androidsidemenu/MainActivity.java
@@ -1,15 +1,45 @@
 package com.aeclarke.androidsidemenu;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // set the content view
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.shadow_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.side_menu);
+
+        ListView menuList = (ListView) findViewById(R.id.menu);
+        ListAdapter adapter;
+        String[] options = {"Option 1", "Option 2"};
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, options);
+        menuList.setAdapter(adapter);
+        ActionBar actionBar = getActionBar();
+        actionBar.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
     }
 }
